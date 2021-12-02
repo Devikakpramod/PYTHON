763cbfb08a65c16317042b2a02ee30e9d2d6d5be
Devikakpramod <kpramoddevika@gmail.com>
2021-12-02 15:07:38 +0530

    first commit

---
+++ methods.py
@@ -0,0 +1,13 @@
+import math as m
+a = 10.2
+b = 20.5
+number = a + b
+print(number)
+print(m.ceil(number))
+print(m.floor(number))
+print(abs(number))
+print(cmp(a,b))
+print(m.exp(number))
+print(m.fabs(number))
+print(m.log(number))
+print(m.log10(number))
